6cdeb2abd7c460fe1f99765d64ab31e0d1d72804
Eric Balaile <eric.balaile@gmail.com>
2026-07-22 05:49:12 +0300

    Organize component architecture

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 ﻿import { Outlet } from 'react-router-dom'
 
-import Navbar from '../components/Navbar.jsx'
-import Footer from '../components/Footer.jsx'
+import Navbar from '../components/layout/Navbar.jsx'
+import Footer from '../components/layout/Footer.jsx'
 
 function MainLayout() {
   return (
