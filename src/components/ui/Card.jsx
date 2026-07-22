da7a834f941befcb6df01237ce08825e58a13f5f
Eric Balaile <eric.balaile@gmail.com>
2026-07-22 07:06:57 +0300

    Create reusable Card UI component

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,27 @@
+﻿import PropTypes from "prop-types";
+
+function Card({
+  children,
+  className = "",
+}) {
+  const cardStyles =
+    "rounded-xl bg-white p-6 shadow-md transition duration-300 hover:shadow-lg";
+
+  const styles = [
+    cardStyles,
+    className,
+  ].join(" ");
+
+  return (
+    <div className={styles}>
+      {children}
+    </div>
+  );
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+export default Card;
