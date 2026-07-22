a9f65fb7e091cc98ebc6cbdacf4cc3bf89b94e66
Eric Balaile <eric.balaile@gmail.com>
2026-07-22 07:05:17 +0300

    Create reusable Section UI component

---
+++ src/components/ui/Section.jsx
@@ -0,0 +1,40 @@
+﻿import PropTypes from "prop-types";
+
+function Section({
+  children,
+  background = "default",
+  className = "",
+}) {
+  const baseStyles =
+    "py-16 md:py-20";
+
+  const backgrounds = {
+    default: "",
+    light: "bg-gray-50",
+    dark: "bg-gray-900 text-white",
+  };
+
+  const sectionStyles = [
+    baseStyles,
+    backgrounds[background],
+    className,
+  ].join(" ");
+
+  return (
+    <section className={sectionStyles}>
+      {children}
+    </section>
+  );
+}
+
+Section.propTypes = {
+  children: PropTypes.node.isRequired,
+  background: PropTypes.oneOf([
+    "default",
+    "light",
+    "dark",
+  ]),
+  className: PropTypes.string,
+};
+
+export default Section;
