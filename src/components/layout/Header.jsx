8521bfb40cfa322a5391c761ceece9c25d50b9a0
Eric Balaile <eric.balaile@gmail.com>
2026-07-22 07:21:15 +0300

    Create reusable Header layout component

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,44 @@
+﻿import Container from "../ui/Container.jsx";
+import Navigation from "./Navigation.jsx";
+import Button from "../ui/Button.jsx";
+
+function Header() {
+  const links = [
+    {
+      label: "Home",
+      path: "/",
+    },
+    {
+      label: "Services",
+      path: "/services",
+    },
+    {
+      label: "About",
+      path: "/about",
+    },
+    {
+      label: "Contact",
+      path: "/contact",
+    },
+  ];
+
+  return (
+    <header>
+      <Container>
+        <div>
+          <div>
+            Global Work & Travel
+          </div>
+
+          <Navigation links={links} />
+
+          <Button variant="primary">
+            Get Started
+          </Button>
+        </div>
+      </Container>
+    </header>
+  );
+}
+
+export default Header;
