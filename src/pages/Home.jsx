92f3669f7c3d075e6a45e4354a1691d79ec9e74e
Eric Balaile <eric.balaile@gmail.com>
2026-07-22 07:08:53 +0300

    Build homepage hero section structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
 ﻿import Container from "../components/ui/Container.jsx";
+import Section from "../components/ui/Section.jsx";
+import Button from "../components/ui/Button.jsx";
 
 function Home() {
   return (
     <main>
-      <Container>
-        <h1>Home Page</h1>
-      </Container>
+      <Section>
+        <Container>
+          <div>
+            <h1>
+              Global Opportunities Start Here
+            </h1>
+
+            <p>
+              Connecting people with international
+              work, travel, and career opportunities.
+            </p>
+
+            <div>
+              <Button variant="primary">
+                Explore Opportunities
+              </Button>
+            </div>
+          </div>
+        </Container>
+      </Section>
     </main>
   );
 }
