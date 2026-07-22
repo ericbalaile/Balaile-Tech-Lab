5bf5945a2e07735afbd89440698759869ab6a943
Eric Balaile <eric.balaile@gmail.com>
2026-07-22 07:20:09 +0300

    Create reusable Navigation layout component

---
+++ src/components/layout/Navigation.jsx
@@ -0,0 +1,30 @@
+﻿import PropTypes from "prop-types";
+
+function Navigation({
+  links = [],
+}) {
+  return (
+    <nav>
+      <ul>
+        {links.map((link) => (
+          <li key={link.label}>
+            <a href={link.path}>
+              {link.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+Navigation.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default Navigation;
